bb10e2a598a8f4719dad7af388eeab9b1d377486
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-05-25 02:08:22 -0400

    Final submission

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -8,17 +8,20 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({id, location, imageSource, username, profile}) => {
   return (
     <div className="photo-list__item">
-      <div className="photos">
-      <img src={imageSource} alt={`Photo ${id}`} className="photo"/>
+      <div className="photo-list-photo">
+      <img src={imageSource} alt={`Photo ${id}`} className="photo-list__image"/>
       </div>
-      <div className="profile-photo">
-      <img src={profile} alt={`${username}'s profile`} className="profile-pic"/>
+      <div>
+      <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile"/>
+      
+      <div className=".photo-list__user-info">
+        <h2 className="photo-list__username"> {username}</h2>
+        <p className="photo-list__location"> {location.city}, {location.country}</p>
       </div>
-      <div className="photo-info">
-        <h2>{username}</h2>
-        <p>{location.city}, {location.country}</p>
       </div>
-    </div>)
+    </div>
+ 
+    )
 
 };
 
