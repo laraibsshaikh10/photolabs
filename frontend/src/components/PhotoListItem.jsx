bdd81daedf22b96484c068a6fbfd06362f850e3d
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-05-25 00:08:39 -0400

    Added styling to the main image in photo-list-items

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -8,9 +8,12 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({id, location, imageSource, username, profile}) => {
   return (
     <div className="photo-list-item">
+      <div className="photos">
       <img src={imageSource} alt={`Photo ${id}`} className="photo"/>
-
+      </div>
+      <div className="profile-photo">
       <img src={profile} alt={`${username}'s profile`} className="profile-pic"/>
+      </div>
       <div className="photo-info">
         <h2>{username}</h2>
         <p>{location.city}, {location.country}</p>
