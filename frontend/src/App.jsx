2170651cbc6eb3bebb9658c9de191cadd532e52d
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-05-28 03:29:51 -0400

    Separation of Concerns activity:created custom hooks file useApplicationData.js to load the initial data from the API, moved the app.js code to the file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,50 +3,20 @@ import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import photosData from 'mocks/photos';
+import useApplicationData from 'assets/hooks/useApplicationData';
 
 const App = () => {
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
-  const [similarPhotos, setSimilarPhotos] = useState([]);
-  const [favourites, setFavourites] = useState([]);
-
-  const toggleFavourite = (photo) => {
-    setFavourites((previousFavourites) => {
-      const isFavourite = previousFavourites.find(fav => fav.id === photo.id);
-      const updatedFavourite = isFavourite
-      ? previousFavourites.filter(fav => fav.id !== photo.id) 
-      : [...previousFavourites, photo];
-      
-      console.log('Favourites updated:', updatedFavourite);
-      return updatedFavourite;
-    });
-  };
-
-  const fetchSimilarPhotos = (photoId) => {
-    // Filter out the selected photo to simulate similar photos
-    return photosData.filter(photo => photo.id !== photoId);
-  };
-
-  const handleOpenModal = (photo) => {
-    const photoDetails = {
-      id: photo.id,
-      url: photo.urls.regular,
-      profilePic: photo.user.profile,
-      username: photo.user.username,
-      userLocation: `${photo.location.city}, ${photo.location.country}`
-    }
-    console.log("Opening modal with photo details:", photoDetails); 
-    setSelectedPhoto(photoDetails);
-    const similarPhotos = fetchSimilarPhotos(photo.id);
-    setSimilarPhotos(similarPhotos);
-    setModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    console.log("Closing modal");  
-    setModalOpen(false);
-    setSelectedPhoto(null);
-  }
+ const {
+  state: {
+    isModalOpen,
+    selectedPhoto,
+    similarPhotos,
+    favourites
+  },
+  handleOpenModal,
+  toggleFavourite,
+  handleCloseModal
+ } = useApplicationData();
   return (
 
     <div className="App">
