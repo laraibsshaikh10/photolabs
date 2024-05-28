ca91fb738252985e6efaaf41f12ff76c52e92ac2
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-05-28 04:23:21 -0400

    Added useReducer functionality

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,63 +1,65 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import photosData from "mocks/photos";
+import reducer, { ACTIONS } from "./useReducerApplicationData";
+
+
+const initialState = {
+  isModalOpen: false,
+  selectedPhoto: null,
+  similarPhotos: [],
+  favourites: [],
+  photosData: photosData
+};
 
 const useApplicationData = () => {
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
-  const [similarPhotos, setSimilarPhotos] = useState([]);
-  const [favourites, setFavourites] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const toggleFavourite = (photo) => {
-    setFavourites((previousFavourites) => {
-      const isFavourite = previousFavourites.find(fav => fav.id === photo.id);
-      const updatedFavourite = isFavourite
-      ? previousFavourites.filter(fav => fav.id !== photo.id) 
-      : [...previousFavourites, photo];
-      
-      console.log('Favourites updated:', updatedFavourite);
-      return updatedFavourite;
-    });
+  //In your toggleFavourite function, you're currently storing the entire photo object in the favourites list. This could potentially use a lot of memory if there are many favourites and the photo objects are large. Consider storing just the photo IDs in the favourites list, as this would be more memory-efficient.
+
+  const toggleFavourite = (photoId) => {
+    // setFavourites((previousFavourites) => {
+      const isFavourite = state.favourites.includes(photoId);
+      dispatch({
+        type: isFavourite? ACTIONS.FAV_PHOTO_REMOVED: ACTIONS.FAV_PHOTO_ADDED,
+        payload: photoId
+      });
+ 
   };
 
-  const fetchSimilarPhotos = (photoId) => {
-    // Filter out the selected photo to simulate similar photos
-    return photosData.filter(photo => photo.id !== photoId);
-  };
+  // const fetchSimilarPhotos = (photoId) => {
+  //   return photosData.filter(photo => photo.id !== photoId);
+  // };
 
   const handleOpenModal = (photo) => {
-    const photoDetails = {
-      id: photo.id,
-      url: photo.urls.regular,
-      profilePic: photo.user.profile,
-      username: photo.user.username,
-      userLocation: `${photo.location.city}, ${photo.location.country}`
-    }
-    console.log("Opening modal with photo details:", photoDetails); 
-    setSelectedPhoto(photoDetails);
-    const similarPhotos = fetchSimilarPhotos(photo.id);
-    setSimilarPhotos(similarPhotos);
-    setModalOpen(true);
+    dispatch({
+      type: ACTIONS.SELECT_PHOTO,
+      payload: {
+        id: photo.id,
+        url: photo.urls.regular,
+        profilePic: photo.user.profile,
+        username: photo.user.username,
+        userLocation: `${photo.location.city}, ${photo.location.country}`
+      }
+    })
+
   };
 
   const handleCloseModal = () => {
-    console.log("Closing modal");  
-    setModalOpen(false);
-    setSelectedPhoto(null);
+    dispatch({
+      type: ACTIONS.CLOSE_PHOTO_MODAL
+    });
+ 
   };
 
   return {
-    state: {
-      isModalOpen,
-      selectedPhoto,
-      similarPhotos,
-      favourites
-    },
-
+    
+    state,
     handleOpenModal,
     toggleFavourite: toggleFavourite,
     handleCloseModal
-  };
+
+  }
 };
 
 export default useApplicationData;
---
+++ frontend/src/hooks/useReducerApplicationData.js
@@ -0,0 +1,63 @@
+/* insert app levels actions below */
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.FAV_PHOTO_ADDED:
+      return {
+        ...state,
+        favourites: [...state.favourites, action.payload]
+      };
+
+    case ACTIONS.FAV_PHOTO_REMOVED:
+      return {
+        ...state,
+        favourites: state.favourites.filter(id => id !==action.payload)
+      };
+
+    case ACTIONS.SET_PHOTO_DATA:
+      return {
+        ...state,
+        photosData: action.payload
+      };
+
+    case ACTIONS.SELECT_PHOTO:
+      return {
+        ...state,
+        selectedPhoto: action.payload,
+        similarPhotos: state.photosData.filter(photo => photo.id !== action.payload.id),
+        isModalOpen: true
+      };
+
+    case ACTIONS.DISPLAY_PHOTO_DETAILS:
+      return {
+        ...state,
+        isModalOpen: true
+      };
+
+    case ACTIONS.CLOSE_PHOTO_MODAL:
+      return {
+        ...state,
+        isModalOpen: false,
+        selectedPhoto: null
+      }
+
+
+    
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+};
+
+export default reducer;
+
+
